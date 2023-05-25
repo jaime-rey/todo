c75018c37b6885222c055f432e2306005ab36308
jaime rey <jaimereycasado@gmail.com>
2023-05-25 21:15:28 +0200

    chapter 2 finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,15 @@ export class AppComponent {
     return this.list.items.filter((item) => !item.complete).length;
   }
   get items(): readonly TodoItem[] {
-    return this.list.items;
+    return this.list.items.filter(
+      (item) => this.showComplete || !item.complete
+    );
   }
+  addItem(newItem: string) {
+    if (newItem != '') {
+      this.list.addItem(newItem);
+    }
+  }
+
+  showComplete: boolean = false;
 }
